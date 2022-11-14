83b597a0d927ac760b11ae083f21926fbb747344
NidzaX <59400424+NidzaX@users.noreply.github.com>
2022-11-14 08:43:16 +0100

    Added Files

---
+++ Vezba7/StudentService_Data/StudentDataRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentService_Data
+{
+    public class StudentDataRepository
+    {
+        private CloudTable _table;
+        private CloudStorageAccount _storageAccount;
+
+        public StudentDataRepository()
+        {
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("DataConnectionString"));
+            CloudTableClient tableClient
+                = new CloudTableClient(new Uri(_storageAccount.TableEndpoint.AbsoluteUri), _storageAccount.Credentials);
+            _table = tableClient.GetTableReference("StudentTable");
+            _table.CreateIfNotExists();
+        }
+
+        public IQueryable<Student> RetrieveAllStudents()
+        {
+            var results = from g in _table.CreateQuery<Student>()
+                          where g.PartitionKey == "Student"
+                          select g;
+
+            return results;
+        }
+
+        public Student GetStudent(string rowKey)
+        {
+            var results = from g in _table.CreateQuery<Student>()
+                          where (g.PartitionKey == "Student" && g.RowKey == rowKey)
+                          select g;
+
+            return results.FirstOrDefault();
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            TableOperation replaceOperation = TableOperation.Replace(student);
+            _table.Execute(replaceOperation);
+        }
+
+        public void AddStudent(Student newStudent)
+        {
+            TableOperation insertOperation = TableOperation.Insert(newStudent);
+            _table.Execute(insertOperation);
+        }
+
+        public bool Exists(string rowKey)
+        {
+            var results = from g in _table.CreateQuery<Student>()
+                          where (g.PartitionKey == "Student" && g.RowKey == rowKey)
+                          select g;
+
+            return results.ToList().Count > 0;
+        }
+    }
+}
---
+++ Vezba7/StudentService_WebRole/Controllers/StudentController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using StudentService_Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentService_WebRole.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDataRepository repo = new StudentDataRepository();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(repo.RetrieveAllStudents());
+        }
+
+        public ActionResult Create()
+        {
+            return View("AddEntity");
+        }
+
+        [HttpPost]
+        public ActionResult AddEntity(string RowKey, string Name, string LastName, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (repo.Exists(RowKey))
+                {
+                    ViewBag.Message = "Student sa unetim indeksom vec postoji!";
+                    return View();
+                }
+
+                BlobHelper blobHelper = new BlobHelper();
+                CloudBlockBlob blob = blobHelper.GetBlockBlobReference("vezba", $"image_{RowKey}");
+                blob.Properties.ContentType = file.ContentType;
+                blobHelper.UploadImage(new Bitmap(file.InputStream), blob);
+
+                Student entry = new Student(RowKey)
+                {
+                    Name = Name,
+                    LastName = LastName,
+                    PhotoUrl = blob.Uri.ToString(),
+                    ThumbnailUrl = blob.Uri.ToString()
+                };
+                repo.AddStudent(entry);
+
+                CloudQueue queue = QueueHelper.GetQueueReference("vezba");
+                queue.AddMessage(new CloudQueueMessage(RowKey));
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("AddEntity");
+            }
+        }
+    }
+}
